ccdfa4dd651a475aac073f5b5daaa8f1b9006579
Duke <dukemorise33@gmail.com>
2023-08-11 11:12:07 -0700

    completed scopes lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'bob the builder';
+
+(function (){
+    return customerName;
+}());
+
+function upperCaseCustomerName(){
+    customerName = 'BOB';
+    console.log(customerName);
+    return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+//don't want to declare a variable within a function.
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'new name';
+    return leastFavoriteCustomer;
+}
